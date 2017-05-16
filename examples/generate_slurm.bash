2c1c4b17d84974b323e02279452e2039210d4d50
Matteo <matteo.pirotta@polimi.it>
2017-05-16 23:31:58 +0200

    Added generation of slurm files

---
+++ examples/generate_slurm.bash
@@ -0,0 +1,95 @@
+#!/bin/bash
+folder=test_$(date '+%Y%m%d_%H%M%S')
+echo ${folder}
+mkdir ${folder}
+
+
+dim=20
+duration=40000000
+tmax=8
+repetitions=5
+init_seed=28632376383341475136
+rmax=${dim}
+
+# CREATE CONFIGURATION FOR FSUCRL
+i=1
+for (( t=1; t<=${tmax}; t++ ))
+do
+    fname=${folder}/fsucrl1_${dim}_${t}.slurm
+    echo "#!/bin/bash" > ${fname}                                                                                                      
+    echo "#SBATCH --nodes=1" >> ${fname}
+    echo "#SBATCH --ntasks-per-node=1" >> ${fname}
+    echo "#SBATCH --cpus-per-task=24" >> ${fname}
+    echo "#SBATCH --time=24:00:00" >> ${fname}
+    echo "#SBATCH --job-name=fsucrl1_${dim}_${t}" >> ${fname}
+    echo "#SBATCH --mem=24G" >> ${fname}
+    echo "#SBATCH --output=fsucrl1_${dim}_${t}_%j.out" >> ${fname}
+    echo "pwd; hostname; date" >> ${fname}
+    echo "" >> ${fname}
+    echo "module load anaconda3/4.1.0" >> ${fname}
+    echo "export OMP_NUM_THREADS=\$SLURM_CPUS_PER_TASK" >> ${fname}
+    echo "export NUMEXPR_NUM_THREADS=\$SLURM_CPUS_PER_TASK" >> ${fname}
+    
+    echo "python ../example_free_smdp.py -d ${dim} -n ${duration} --tmax ${t} --rmax ${rmax} -r ${repetitions} --seed ${init_seed} --id c${i}" >> ${fname}
+    i=$((i+1))
+    echo "python ../example_free_smdp.py -b -d ${dim} -n ${duration} --tmax ${t} --rmax ${rmax} -r ${repetitions} --seed ${init_seed} --id c${i}" >> ${fname} #bernstein
+    i=$((i+1))
+    
+    cd ${folder}
+    sbatch ${fname}
+    cd ..
+    sleep 1
+done
+
+# CREATE CONFIGURATION FOR SUCRL
+i=1
+for (( t=1; t<=${tmax}; t++ ))
+do
+    fname=${folder}/sucrl_${dim}_${t}.slurm
+    echo "#!/bin/bash" > ${fname}                                                                                                      
+    echo "#SBATCH --nodes=1" >> ${fname}
+    echo "#SBATCH --ntasks-per-node=1" >> ${fname}
+    echo "#SBATCH --cpus-per-task=24" >> ${fname}
+    echo "#SBATCH --time=24:00:00" >> ${fname}
+    echo "#SBATCH --job-name=sucrl_${dim}_${t}" >> ${fname}
+    echo "#SBATCH --mem=24G" >> ${fname}
+    echo "#SBATCH --output=sucrl_${dim}_${t}_%j.out" >> ${fname}
+    echo "pwd; hostname; date" >> ${fname}
+    echo "" >> ${fname}
+    echo "module load anaconda3/4.1.0" >> ${fname}
+    echo "export OMP_NUM_THREADS=\$SLURM_CPUS_PER_TASK" >> ${fname}
+    echo "export NUMEXPR_NUM_THREADS=\$SLURM_CPUS_PER_TASK" >> ${fname}
+    
+    echo "python ../example_smdp.py -d ${dim} -n ${duration} --tmax ${t} --rmax ${rmax} -r ${repetitions} --seed ${init_seed} --id c${i}" >> ${fname}
+    i=$((i+1))
+    echo "python ../example_smdp.py -b -d ${dim} -n ${duration} --tmax ${t} --rmax ${rmax} -r ${repetitions} --seed ${init_seed} --id c${i}" >> ${fname} #bernstein
+    i=$((i+1))
+    
+    cd ${folder}
+    sbatch ${fname}
+    cd ..
+    sleep 1
+done
+
+# CREATE CONFIGURATION FOR MDP
+fname=${folder}/mdpucrl_${dim}_${t}.slurm
+echo "#!/bin/bash" > ${fname}                                                                                                      
+echo "#SBATCH --nodes=1" >> ${fname}
+echo "#SBATCH --ntasks-per-node=1" >> ${fname}
+echo "#SBATCH --cpus-per-task=24" >> ${fname}
+echo "#SBATCH --time=24:00:00" >> ${fname}
+echo "#SBATCH --job-name=mdpucrl_${dim}_${t}" >> ${fname}
+echo "#SBATCH --mem=24G" >> ${fname}
+echo "#SBATCH --output=mdpucrl_${dim}_${t}_%j.out" >> ${fname}
+echo "pwd; hostname; date" >> ${fname}
+echo "" >> ${fname}
+echo "module load anaconda3/4.1.0" >> ${fname}
+echo "export OMP_NUM_THREADS=\$SLURM_CPUS_PER_TASK" >> ${fname}
+echo "export NUMEXPR_NUM_THREADS=\$SLURM_CPUS_PER_TASK" >> ${fname}
+
+echo "python ../example_mdp.py -d ${dim} -n ${duration} --rmax ${rmax} -r ${repetitions} --seed ${init_seed} --id c1" >> ${fname}
+echo "python ../example_mdp.py -b -d ${dim} -n ${duration} --rmax ${rmax} -r ${repetitions} --seed ${init_seed} --id c2" >> ${fname} #bernstein
+
+cd ${folder}
+sbatch ${fname}
+cd ..
