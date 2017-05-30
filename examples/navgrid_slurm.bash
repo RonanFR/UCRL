6d49cd14ff75d23ef531c96e849ed10a44cb7b44
Matteo <matteo.pirotta@polimi.it>
2017-05-30 20:49:09 +0200

    Updated

--- examples/navgrid_slurm.bash
+++ examples/navgrid_slurm.bash
@@ -1,134 +1,59 @@
 #!/bin/bash
 N_cpu=16
 N_mem=50g
+N_hours=24
 
 dim=20
 duration=80000000
 repetitions=1
 init_seed=114364114
 rmax=${dim}
-tmax=${dim} #$((1+dim/2))
+tmax=$((dim/2))
+exe_file=../example_navgrid.py 
 
 
 folder=navgrid_${dim}_$(date '+%Y%m%d_%H%M%S')
 echo ${folder}
 mkdir ${folder}
 
-# CREATE CONFIGURATION FOR FSUCRL
 
-#v1
-i=1
-for (( t=1; t<=${tmax}; t++ ))
+ALGS=(FSUCRLv1 FSUCRLv2 SUCRL UCRL)
+A_SHORT_NAME=(N-fv1 N-fv2 N-suc N-ucr)
+
+# CREATE CONFIGURATIONS
+
+for (( j=0; j<${#ALGS[@]}; j++ ))
 do
-    sname=fsucrlv1.slurm
-    fname=${folder}/${sname}
-    echo "#!/bin/bash" > ${fname}                                                                                                      
-    echo "#SBATCH --nodes=1" >> ${fname}
-    echo "#SBATCH --ntasks-per-node=1" >> ${fname}
-    echo "#SBATCH --cpus-per-task=${N_cpu}" >> ${fname}
-    echo "#SBATCH --time=24:00:00" >> ${fname}
-    echo "#SBATCH --job-name=gf1_${dim}_${t}" >> ${fname}
-    echo "#SBATCH --mem=${N_mem}" >> ${fname}
-    echo "#SBATCH --output=fsucrlv1_${dim}_${t}_%j.out" >> ${fname}
-    echo "pwd; hostname; date" >> ${fname}
-    echo "" >> ${fname}
-    echo "module load anaconda3/4.1.0" >> ${fname}
-    echo "export OMP_NUM_THREADS=\$SLURM_CPUS_PER_TASK" >> ${fname}
-    echo "export NUMEXPR_NUM_THREADS=\$SLURM_CPUS_PER_TASK" >> ${fname}
+    echo ${j} ${ALGS[$j]}
+    i=1
+    for (( t=1; t<=${tmax}; t++ ))
+    do
+        out_name="${ALGS[$j]}_${dim}_${t}_%j.out"
+        sname=${ALGS[$j]}_${dim}_${t}.slurm
+        fname=${folder}/${sname}
+        
+        echo "#!/bin/bash" > ${fname}                                                                                                      
+        echo "#SBATCH --nodes=1" >> ${fname}
+        echo "#SBATCH --ntasks-per-node=1" >> ${fname}
+        echo "#SBATCH --cpus-per-task=${N_cpu}" >> ${fname}
+        echo "#SBATCH --time=${N_hours}:00:00" >> ${fname}
+        echo "#SBATCH --job-name=${A_SHORT_NAME[$j]}_${dim}_${t}" >> ${fname}
+        echo "#SBATCH --mem=${N_mem}" >> ${fname}
+        echo "#SBATCH --output=${out_name}" >> ${fname}
+        echo "pwd; hostname; date" >> ${fname}
+        echo "" >> ${fname}
+        echo "module load anaconda3/4.1.0" >> ${fname}
+        echo "export OMP_NUM_THREADS=\$SLURM_CPUS_PER_TASK" >> ${fname}
+        echo "export NUMEXPR_NUM_THREADS=\$SLURM_CPUS_PER_TASK" >> ${fname}
 
-    echo "python ../example_navgrid.py --alg FSUCRLv1 -d ${dim} -n ${duration} --tmax ${t} --rmax ${rmax} -r ${repetitions} --seed ${init_seed} --id c${i}" >> ${fname}
-    i=$((i+1))
-    echo "python ../example_navgrid.py -b --alg FSUCRLv1 -d ${dim} -n ${duration} --tmax ${t} --rmax ${rmax} -r ${repetitions} --seed ${init_seed} --id c${i}" >> ${fname}
+        echo "python ${exe_file} --alg FSUCRLv1 -d ${dim} -n ${duration} --tmax ${t} --rmax ${rmax} -r ${repetitions} --seed ${init_seed} --id c${i}" >> ${fname}
+        i=$((i+1))
+        echo "python ${exe_file} -b --alg FSUCRLv1 -d ${dim} -n ${duration} --tmax ${t} --rmax ${rmax} -r ${repetitions} --seed ${init_seed} --id c${i}" >> ${fname}
 
-    cd ${folder}
-    sbatch ${sname}
-    cd ..
-    sleep 1
+        cd ${folder}
+        sbatch ${sname}
+        cd ..
+        sleep 1
+
+    done
 done
-
-#v2
-i=1
-for (( t=1; t<=${tmax}; t++ ))
-do
-    sname=fsucrlv2.slurm
-    fname=${folder}/${sname}
-    echo "#!/bin/bash" > ${fname}                                                                                                      
-    echo "#SBATCH --nodes=1" >> ${fname}
-    echo "#SBATCH --ntasks-per-node=1" >> ${fname}
-    echo "#SBATCH --cpus-per-task=${N_cpu}" >> ${fname}
-    echo "#SBATCH --time=24:00:00" >> ${fname}
-    echo "#SBATCH --job-name=gf2_${dim}_${t}" >> ${fname}
-    echo "#SBATCH --mem=${N_mem}" >> ${fname}
-    echo "#SBATCH --output=fsucrlv2_${dim}_${t}_%j.out" >> ${fname}
-    echo "pwd; hostname; date" >> ${fname}
-    echo "" >> ${fname}
-    echo "module load anaconda3/4.1.0" >> ${fname}
-    echo "export OMP_NUM_THREADS=\$SLURM_CPUS_PER_TASK" >> ${fname}
-    echo "export NUMEXPR_NUM_THREADS=\$SLURM_CPUS_PER_TASK" >> ${fname}
-
-    echo "python ../example_navgrid.py --alg FSUCRLv2 -d ${dim} -n ${duration} --tmax ${t} --rmax ${rmax} -r ${repetitions} --seed ${init_seed} --id c${i}" >> ${fname}
-    i=$((i+1))
-    echo "python ../example_navgrid.py -b --alg FSUCRLv2 -d ${dim} -n ${duration} --tmax ${t} --rmax ${rmax} -r ${repetitions} --seed ${init_seed} --id c${i}" >> ${fname}
-
-    cd ${folder}
-    sbatch ${sname}
-    cd ..
-    sleep 1
-done
-
-# CREATE CONFIGURATION FOR SUCRL
-i=1
-for (( t=1; t<=${tmax}; t++ ))
-do
-    sname=sucrl.slurm
-    fname=${folder}/${sname}
-    echo "#!/bin/bash" > ${fname}                                                                                                      
-    echo "#SBATCH --nodes=1" >> ${fname}
-    echo "#SBATCH --ntasks-per-node=1" >> ${fname}
-    echo "#SBATCH --cpus-per-task=${N_cpu}" >> ${fname}
-    echo "#SBATCH --time=24:00:00" >> ${fname}
-    echo "#SBATCH --job-name=gsu_${dim}_${t}" >> ${fname}
-    echo "#SBATCH --mem=${N_mem}" >> ${fname}
-    echo "#SBATCH --output=sucrl_${dim}_${t}_%j.out" >> ${fname}
-    echo "pwd; hostname; date" >> ${fname}
-    echo "" >> ${fname}
-    echo "module load anaconda3/4.1.0" >> ${fname}
-    echo "export OMP_NUM_THREADS=\$SLURM_CPUS_PER_TASK" >> ${fname}
-    echo "export NUMEXPR_NUM_THREADS=\$SLURM_CPUS_PER_TASK" >> ${fname}
-
-    echo "python ../example_navgrid.py --alg SUCRL -d ${dim} -n ${duration} --tmax ${t} --rmax ${rmax} -r ${repetitions} --seed ${init_seed} --id c${i}" >> ${fname}
-    i=$((i+1))
-    echo "python ../example_navgrid.py -b --alg SUCRL -d ${dim} -n ${duration} --tmax ${t} --rmax ${rmax} -r ${repetitions} --seed ${init_seed} --id c${i}" >> ${fname}
-
-    cd ${folder}
-    sbatch ${sname}
-    cd ..
-    sleep 1
-done
-
-# CREATE CONFIGURATION FOR MDP
-i=1
-sname=ucrl.slurm
-fname=${folder}/${sname}
-echo "#!/bin/bash" > ${fname}                                                                                                      
-echo "#SBATCH --nodes=1" >> ${fname}
-echo "#SBATCH --ntasks-per-node=1" >> ${fname}
-echo "#SBATCH --cpus-per-task=${N_cpu}" >> ${fname}
-echo "#SBATCH --time=24:00:00" >> ${fname}
-echo "#SBATCH --job-name=guc_${dim}" >> ${fname}
-echo "#SBATCH --mem=${N_mem}" >> ${fname}
-echo "#SBATCH --output=mdpucrl_${dim}_%j.out" >> ${fname}
-echo "pwd; hostname; date" >> ${fname}
-echo "" >> ${fname}
-echo "module load anaconda3/4.1.0" >> ${fname}
-echo "export OMP_NUM_THREADS=\$SLURM_CPUS_PER_TASK" >> ${fname}
-echo "export NUMEXPR_NUM_THREADS=\$SLURM_CPUS_PER_TASK" >> ${fname}
-
-echo "python ../example_roommaze.py --alg UCRL -d ${dim} -n ${duration} --tmax -1 --rmax ${rmax} -r ${repetitions} --seed ${init_seed} --id c${i}" >> ${fname}
-i=$((i+1))
-echo "python ../example_roommaze.py -b --alg UCRL -d ${dim} -n ${duration} --tmax -1 --rmax ${rmax} -r ${repetitions} --seed ${init_seed} --id c${i}" >> ${fname}
-
-cd ${folder}
-sbatch ${sname}
-cd ..
-sleep 1
