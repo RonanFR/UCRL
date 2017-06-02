8ab582c9abfd01a79f6092b2d5b31b249d0fd624
Matteo <matteo.pirotta@polimi.it>
2017-06-02 12:01:30 +0200

    Updated

--- examples/navgrid_parallel.bash
+++ examples/navgrid_parallel.bash
@@ -7,13 +7,13 @@ init_seed=114364114
 rmax=1 #${dim}
 tmax=8
 
-folder=rooms_${dim}_$(date '+%Y%m%d_%H%M%S')
+folder=navgrid_${dim}_$(date '+%Y%m%d_%H%M%S')
 echo ${folder}
 mkdir ${folder}
 
 TMAX_SEQ=$(seq 1 ${tmax})
 N_PARALLEL="10"
-COMMAND="python ../example_roommaze.py"
+COMMAND="python ../example_navgrid.py"
 ALGS="FSUCRLv1 FSUCRLv2 SUCRL UCRL"
 opt_alpha=" --p_alpha 0.02 --mc_alpha 0.02 --r_alpha 0.8 --tau_alpha 0.8 "
 fix_options=" -d ${dim} -n ${duration} --rmax ${rmax} -r ${repetitions} --seed ${init_seed} "
