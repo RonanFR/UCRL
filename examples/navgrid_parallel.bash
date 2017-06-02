bcdeee002310fa389bb499b1f8254fcf220f6281
Matteo <matteo.pirotta@polimi.it>
2017-06-02 11:58:25 +0200

    Updated

--- examples/navgrid_parallel.bash
+++ examples/navgrid_parallel.bash
@@ -1,22 +1,24 @@
  #!/bin/bash
 
-dim=14
-duration=60000000
-repetitions=2
+dim=20
+duration=40000000
+repetitions=1
 init_seed=114364114
 rmax=1 #${dim}
+tmax=8
 
 folder=rooms_${dim}_$(date '+%Y%m%d_%H%M%S')
 echo ${folder}
 mkdir ${folder}
 
+TMAX_SEQ=$(seq 1 ${tmax})
 N_PARALLEL="10"
 COMMAND="python ../example_roommaze.py"
 ALGS="FSUCRLv1 FSUCRLv2 SUCRL UCRL"
 opt_alpha=" --p_alpha 0.02 --mc_alpha 0.02 --r_alpha 0.8 --tau_alpha 0.8 "
-fix_options=" -d ${dim} -n ${duration} --rmax ${rmax} -r ${repetitions} --seed ${init_seed} --id c1"
+fix_options=" -d ${dim} -n ${duration} --rmax ${rmax} -r ${repetitions} --seed ${init_seed} "
 
 cd $folder
 export OMP_NUM_THREADS=12
 export NUMEXPR_NUM_THREADS=12
-parallel -j $N_PARALLEL $COMMAND ${opt_alpha} ${fix_options} --alg ${1} ::: ${ALGS}
+parallel -j $N_PARALLEL echo $COMMAND ${opt_alpha} ${fix_options} --alg {1} --tmax {2} --id c{2} ::: ${ALGS} ::: ${TMAX_SEQ}
