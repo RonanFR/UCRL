64532d2703ecf4ec981ea12d59719041b21c9613
Matteo <matteo.pirotta@polimi.it>
2017-06-02 11:27:11 +0200

    Added parallel script

---
+++ examples/4rooms_parallel.bash
@@ -0,0 +1,22 @@
+ #!/bin/bash
+
+dim=14
+duration=60000000
+repetitions=2
+init_seed=114364114
+rmax=1 #${dim}
+
+folder=rooms_${dim}_$(date '+%Y%m%d_%H%M%S')
+echo ${folder}
+mkdir ${folder}
+
+N_PARALLEL="10"
+COMMAND="python ../example_roommaze.py"
+ALGS="FSUCRLv1 FSUCRLv2 SUCRL UCRL"
+opt_alpha=" --p_alpha 0.02 --mc_alpha 0.02 --r_alpha 0.8 --tau_alpha 0.8 "
+fix_options=" -d ${dim} -n ${duration} --rmax ${rmax} -r ${repetitions} --seed ${init_seed} -id c1"
+
+cd $folder
+export OMP_NUM_THREADS=12
+export NUMEXPR_NUM_THREADS=12
+parallel -j $N_PARALLEL $COMMAND --alg ${1} ${ALPHAS}  --id c${i} ::: ${ALGS}
