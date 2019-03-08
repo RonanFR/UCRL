cb6943d0e900efe3424660b9d46f0809a0d48541
Matteo Pirotta <teopir@users.noreply.github.com>
2019-03-08 14:24:51 +0100

    Added __init__

---
+++ rlexplorer/evi/__init__.py
@@ -0,0 +1,11 @@
+from .evi import EVI
+from .free_evi import EVI_FSUCRLv1, EVI_FSUCRLv2
+from .scevi import SpanConstrainedEVI
+from .tevi import TEVI
+from .tevi2 import TEVI2
+
+__all__ = ["EVI",
+           "EVI_FSUCRLv1",
+           "EVI_FSUCRLv2",
+           "SpanConstrainedEVI",
+           "TEVI", "TEVI2"]
