b245f74f610cb1b2a1e44918ac4317203495110b
Matteo Pirotta <teopir@users.noreply.github.com>
2019-04-22 17:52:47 +0200

    fixed range for cartpole since it was inf

--- examples/solve_contmdp_via_grid.py
+++ examples/solve_contmdp_via_grid.py
@@ -13,7 +13,7 @@ print("This is the name of the script: {}".format(sys.argv[0]))
 
 if len(sys.argv) == 1:
     print("Running MountainCar test")
-    env_name = "POLE"# ["MC", "POLE", "ACROBOT"]
+    env_name = "POLE"  # ["MC", "POLE", "ACROBOT"]
     path = None
 else:
     env_name = None
@@ -34,13 +34,38 @@ if env_name is None:
     S = data['S']
     A = data['A']
 
-    P = np.zeros((S,A,S))
+    P = np.zeros((S, A, S))
     P[Pidx] = Pnnz
-    R = np.zeros((S,A))
+    R = np.zeros((S, A))
     R[Ridx] = Rnnz
 
-    print(P)
-    print(R)
+    print(Rnnz)
+
+    env_name = data['env_name']
+    if env_name == "MC":
+        env = MountainCarEnv()
+    elif env_name == "POLE":
+        env = CartPoleEnv()
+    elif env_name == "ACROBOT":
+        env = AcrobotEnv
+
+    bins = data['bins']
+    grid = Discretizer(bins=bins)
+
+    # state_actions = [list(range(A)) for _ in range(S)]
+    #
+    # policy_indices = 99*np.ones((S,), dtype=np.int)
+    # policy = 99*np.ones((S,), dtype=np.int)
+    # gain, u2 = value_iteration(policy_indices,
+    #                     policy,
+    #                     S, state_actions,
+    #                     P, R,
+    #                     1e-10, 0.9)
+    #
+    # print("SPAN: {}".format(np.max(u2)-np.min(u2)))
+    # print("GAIN: {}".format(gain))
+
+
 
 else:
     if env_name == "POLE":
@@ -49,8 +74,8 @@ else:
         Nbins = 15
         Nsamples = 1
 
-        LM = env.observation_space.low
-        HM = env.observation_space.high
+        HM = np.array([1.2 * env.x_threshold, 2., 1.2 * env.theta_threshold_radians, 3.])
+        LM = -HM
         D = len(HM)
 
         bins = []
@@ -59,11 +84,11 @@ else:
             V = np.linspace(LM[i], HM[i], Nbins)
             bins.append(V[1:-1])
 
-            V = np.linspace(LM[i], HM[i], 3*Nbins)
+            V = np.linspace(LM[i], HM[i], 3 * Nbins)
             points.append(V)
 
         samples = np.meshgrid(*points)
-        samples = np.concatenate([M.reshape(-1,1) for M in samples], axis=1)
+        samples = np.concatenate([M.reshape(-1, 1) for M in samples], axis=1)
 
         grid = Discretizer(bins=bins)
 
@@ -86,12 +111,11 @@ else:
             V = np.linspace(LM[i], HM[i], Nbins)
             bins.append(V[1:-1])
 
-            V = np.linspace(LM[i], HM[i], 7*Nbins)
+            V = np.linspace(LM[i], HM[i], 7 * Nbins)
             points.append(V)
 
-
         samples = np.meshgrid(*points)
-        samples = np.concatenate([M.reshape(-1,1) for M in samples], axis=1)
+        samples = np.concatenate([M.reshape(-1, 1) for M in samples], axis=1)
 
         grid = Discretizer(bins=bins)
         S, A = grid.n_bins(), env.action_space.n
@@ -113,12 +137,11 @@ else:
             V = np.linspace(LM[i], HM[i], Nbins)
             bins.append(V[1:-1])
 
-            V = np.linspace(LM[i], HM[i], 3*Nbins)
+            V = np.linspace(LM[i], HM[i], 3 * Nbins)
             points.append(V)
 
-
         samples = np.meshgrid(*points)
-        samples = np.concatenate([M.reshape(-1,1) for M in samples], axis=1)
+        samples = np.concatenate([M.reshape(-1, 1) for M in samples], axis=1)
 
         grid = Discretizer(bins=bins)
         S, A = grid.n_bins(), env.action_space.n
@@ -126,7 +149,6 @@ else:
     P = np.zeros((S, A, S))
     R = np.zeros((S, A))
     Nas = np.zeros((S, A))
-
 
     reach_graph = [[] for _ in range(S)]
 
@@ -153,8 +175,10 @@ else:
                 R[sd, a] += reward
                 Nas[sd, a] += 1
 
-    P = P / Nas[:, :, np.newaxis]
-    R = R / Nas
+    N = np.maximum(Nas, 1)
+
+    P = P / N[:, :, np.newaxis]
+    R = R / N
 
     initial_state = np.asscalar(grid.dpos(env.reset()))
     Rs = []
@@ -170,13 +194,12 @@ else:
     print("Reachable states: {}".format(Rs))
     print("reach {} / tot {}".format(len(Rs), grid.n_bins()))
 
-
     assert np.allclose(np.sum(P, axis=-1), 1)
 
     print(P)
     print(R)
 
-    id='{:%Y%m%d_%H%M%S}_{}_{}'.format(datetime.datetime.now(), Nbins, Nsamples)
+    id = '{:%Y%m%d_%H%M%S}_{}_{}'.format(datetime.datetime.now(), Nbins, Nsamples)
     pickle_name = "{}_{}.pickle".format(env_name, id)
     folder_results = './'
     with open(os.path.join(folder_results, pickle_name), 'wb') as f:
@@ -187,25 +210,25 @@ else:
         Ridx = np.nonzero(R)
         Rnnz = R[Ridx]
 
-        pickle.dump({"Pnnz": Pnnz, "Rnnz": Rnnz, "Pidx": Pidx, "Ridx": Ridx, "S": S, "A": A}, f)
+        pickle.dump({"Pnnz": Pnnz, "Rnnz": Rnnz, "Pidx": Pidx, "Ridx": Ridx, "S": S, "A": A, "env_name": env_name,
+                     "bins": bins}, f)
 
     state_actions = [list(range(A)) for _ in range(S)]
 
-    policy_indices = 99*np.ones((S,), dtype=np.int)
-    policy = 99*np.ones((S,), dtype=np.int)
+    policy_indices = 99 * np.ones((S,), dtype=np.int)
+    policy = 99 * np.ones((S,), dtype=np.int)
     gain, u2 = value_iteration(policy_indices,
-                        policy,
-                        S, state_actions,
-                        P, R,
-                        1e-10, 1)
+                               policy,
+                               S, state_actions,
+                               P, R,
+                               1e-10, 1)
 
-    print("SPAN: {}".format(np.max(u2)-np.min(u2)))
+    print("SPAN: {}".format(np.max(u2) - np.min(u2)))
     print("GAIN: {}".format(gain))
-
 
     if False:
         Preach = P[Rs]
-        Preach = Preach[:,:,Rs]
+        Preach = Preach[:, :, Rs]
         print(Preach.shape)
 
         print("Computing Diameter ...")
@@ -213,58 +236,62 @@ else:
         print("diameter: {}".format(diameter))
 
 
-    trew = 0
-    observation = env.reset()
-    steps = 1000
-    nb_episodes = 0
-    for _ in range(steps):
-        env.render()
-        xd = grid.dpos(observation)
-        action = np.asscalar(policy[xd])
-        observation, reward, done, info = env.step(action)
+# --------------------------------------------------------------------------------------------------------------
+# Execute policy and visualize it
+# --------------------------------------------------------------------------------------------------------------
+trew = 0
+observation = env.reset()
+steps = 1000
+nb_episodes = 0
+for _ in range(steps):
+    env.render()
+    xd = grid.dpos(observation)
+    action = np.asscalar(policy[xd])
+    observation, reward, done, info = env.step(action)
 
-        if done:
-            observation = env.reset()
-            if env_name in ["MC", "POLE"]:
-                reward = 0
-            nb_episodes += 1
+    if done:
+        observation = env.reset()
+        if env_name in ["MC", "POLE"]:
+            reward = 0
+        nb_episodes += 1
 
-        if env_name == "MC":
-            reward += 1
+        break
 
-        trew += reward
+    if env_name == "MC":
+        reward += 1
 
-    env.close()
-    print("avg_reward: {}".format(trew/steps))
-    print("#episodes: {}".format(nb_episodes))
+    trew += reward
 
+env.close()
+print("avg_reward: {}".format(trew / steps))
+print("#episodes: {}".format(nb_episodes))
 
-    # import pickle
-    #
-    #
-    # path = '/Users/pirotta/Documents/Projects/github/UCRL/examples/SCCALPLUS_GymMCWrapper2_20190422_154453/run_0.pickle'
-    # with open(path, 'rb') as f:
-    #     data = pickle.load(f)
-    #
-    # print(data.nb_observations)
-    # print(data.beta_r())
-    #
-    #
-    # trew = 0
-    # observation = env.reset()
-    # steps = 1000
-    # for _ in range(steps):
-    #     env.render()
-    #     xd = np.asscalar(data.environment.grid.dpos(observation))
-    #     # print(observation, xd)
-    #     action = data.sample_action(xd)[1]
-    #     observation, reward, done, info = env.step(action)
-    #
-    #     if done:
-    #         observation = env.reset()
-    #         reward = 0
-    #
-    #     trew += reward
-    #
-    # env.close()
-    # print(trew/steps)
+# import pickle
+#
+#
+# path = '/Users/pirotta/Documents/Projects/github/UCRL/examples/SCCALPLUS_GymMCWrapper2_20190422_154453/run_0.pickle'
+# with open(path, 'rb') as f:
+#     data = pickle.load(f)
+#
+# print(data.nb_observations)
+# print(data.beta_r())
+#
+#
+# trew = 0
+# observation = env.reset()
+# steps = 1000
+# for _ in range(steps):
+#     env.render()
+#     xd = np.asscalar(data.environment.grid.dpos(observation))
+#     # print(observation, xd)
+#     action = data.sample_action(xd)[1]
+#     observation, reward, done, info = env.step(action)
+#
+#     if done:
+#         observation = env.reset()
+#         reward = 0
+#
+#     trew += reward
+#
+# env.close()
+# print(trew/steps)
