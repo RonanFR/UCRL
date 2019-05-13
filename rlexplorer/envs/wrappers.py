709d3f2b12b3f7e325a26a80c254e53b9d229f70
Matteo Pirotta <teopir@users.noreply.github.com>
2019-05-13 23:10:30 +0200

    Updated

--- rlexplorer/envs/wrappers.py
+++ rlexplorer/envs/wrappers.py
@@ -2,7 +2,7 @@ import numpy as np
 from ..evi.evi import EVI
 from .Environment import Environment
 from ..utils.shortestpath import dpshortestpath
-from ..utils.discretization import Discretizer
+from ..utils.discretization import Discretizer, UniformDiscWOB
 from gym.envs.toy_text.taxi import TaxiEnv
 from gym.envs.classic_control import MountainCarEnv, CartPoleEnv
 from .mountaincar import MountainCar
@@ -288,7 +288,7 @@ class GymContinuousWrapper:
             if self.reward_done is not None:
                 self.reward = self.reward_done
         else:
-            self.state = np.asscalar(self.grid.dpos(next_state))
+            self.state = self.grid.dpos(next_state)
 
     def compute_max_gain(self):
         return self.max_gain
@@ -322,11 +322,22 @@ class GymMountainCarWr(GymContinuousWrapper):
         HM = env.observation_space.high
         D = len(HM)
 
+
         bins = []
         for i in range(D):
             V = np.linspace(LM[i], HM[i], Nbins)
             bins.append(V[1:-1])
-        grid = Discretizer(bins=bins)
+        if Nbins == 16:
+            TOREMOVE = [0,11,12,13,14,27,28,29,43,44,59,74,89,104,119,120,134,135,149,150,151,164,165,166,167,178,179,180,181,182,183,193,194,195,196,197,198,199,207,208,209,210,211,212,213,214,215,222,223,224]
+            grid = UniformDiscWOB(bins=bins, binstoremove=TOREMOVE)
+        elif Nbins == 25:
+            TOREMOVE = [0,1,16,17,18,19,20,21,22,23,24,42,43,44,45,46,47,48,67,68,69,70,71,92,93,94,95,117,118,119,141,142,143,166,167,190,191,215,239,263,264,287,288,311,312,313,334,335,336,337,358,359,360,361,362,382,383,384,385,386,387,405,406,407,408,409,410,411,412,429,430,431,432,433,434,435,436,437,452,453,454,455,456,457,458,459,460,461,462,476,477,478,479,480,481,482,483,484,485,486,487,500,501,502,503,504,505,506,507,508,509,510,511,512,523,524,525,526,527,528,529,530,531,532,533,534,535,536,537,547,548,549,550,551,552,553,554,555,556,557,558,559,560,561,562,563,564,565,566,567,568,569,570,571,572,573,574,575]
+            grid = UniformDiscWOB(bins=bins, binstoremove=TOREMOVE)
+        elif Nbins == 30:
+            TOREMOVE =[0,19,20,21,22,23,24,25,26,27,28,29,50,51,52,53,54,55,56,57,80,81,82,83,84,85,86,110,111,112,113,114,115,140,141,142,143,144,170,171,172,173,200,201,202,229,230,231,259,260,288,289,318,34,376,405,406,434,435,463,464,465,492,493,494,521,522,523,524,549,550,551,552,553,554,578,579,580,581,582,583,584,606,607,608,609,610,611,612,613,614,635,636,637,638,639,640,641,642,643,644,664,665,666,667,668,669,670,671,672,673,674,692,693,694,695,696,697,698,699,700,701,702,703,704,721,722,723,724,725,726,727,728,729,730,731,732,733,734,750,751,752,753,754,755,756,757,758,759,760,761,762,763,764,779,780,781,782,783,784,785,786,787,788,789,790,791,792,793,794,795,808,809,810,811,812,813,814,815,816,817,818,819,820,821,822,823,824,825,826,827,828,829,830,831,832,833,834,835,836,837,838,839,840]
+            grid = UniformDiscWOB(bins=bins, binstoremove=TOREMOVE)
+        else:
+            grid = Discretizer(bins=bins)
         super(GymMountainCarWr, self).__init__(
             gym_env=env, grid=grid,
             min_reward=-1, max_reward=0,
@@ -338,7 +349,7 @@ class GymMountainCarWr(GymContinuousWrapper):
 
     def reset(self):
         next_state = self.gym_env.reset()
-        self.state = np.asscalar(self.grid.dpos(next_state))
+        self.state = self.grid.dpos(next_state)
 
 
 class ShipSteeringWr(GymContinuousWrapper):
@@ -367,7 +378,7 @@ class ShipSteeringWr(GymContinuousWrapper):
 
     def reset(self):
         next_state = self.gym_env.reset()
-        self.state = np.asscalar(self.grid.dpos(next_state))
+        self.state = self.grid.dpos(next_state)
 
 
 class PuddleWorldWr(GymContinuousWrapper):
